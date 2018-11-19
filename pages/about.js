b98eaf4ceb4438023726a5c90e5029ab5445e55c
Stefan Andrei <picotto@Stefans-MacBook-Pro.local>
2018-11-19 13:39:00 +0100

    header + navigation component created

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Header from "../components/header";
+import Navigation from "../components/navigation";
+
 export default () => (
   <div>
+    <Header />
+    <Navigation />
     <p>This is the about page</p>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,21 @@
-import Link from "next/link";
+import Navigation from "../components/navigation";
+import Header from "../components/header";
 
 const Index = () => (
   <div>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-    <header>
-      <h1>Header H1</h1>
-    </header>
+    <Header />
+    <Navigation />
     <div className="div-paragraph">
       <p>First paragraph</p>
       <p>Second paragraph</p>
     </div>
     <style jsx>{`
-      a {
-        margin-right: 15px;
-      }
-
       .div-paragraph {
         display: grid;
         grid-template-columns: 1fr 1fr;
       }
-
       * {
-        font-size: 20px;
         text-align: center;
-      }
-
-      p {
-        color: red;
-        font-size: 14px;
       }
     `}</style>
   </div>
